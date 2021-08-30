3569937320df8a978d242da8feb2c5ca1c8fd2d7
lucasjoviniano <lucasjoviniano1@gmail.com>
2021-08-30 20:55:06 -0300

    Add Form Validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,4 @@
 class Answer < ApplicationRecord
+  validates :statement, presence: true
   belongs_to :question
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,11 @@
+# frozen_string_literal: true
+
 class Question < ApplicationRecord
-  has_many :answers, :dependent => :destroy
+  has_many :answers, dependent: :destroy
   accepts_nested_attributes_for :answers
   belongs_to :area
+  validates :statement, :num, presence: true
+  validates :year,
+            numericality: { only_integer: true, greater_than_or_equal_to: 2002, less_than: Time.new.year,
+                            other_than: 2020, other_than: 2021 }
 end
